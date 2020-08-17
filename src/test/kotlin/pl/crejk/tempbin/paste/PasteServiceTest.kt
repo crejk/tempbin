5d39f93c48fd0d9e986c1c101b33025dc6bd3dbd
crejk <crejk@ctemplar.com>
2020-08-17 17:08:28 +0200

    improving service test

--- src/test/kotlin/pl/crejk/tempbin/paste/PasteServiceTest.kt
+++ src/test/kotlin/pl/crejk/tempbin/paste/PasteServiceTest.kt
@@ -2,15 +2,31 @@ package pl.crejk.tempbin.paste
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 import pl.crejk.tempbin.paste.repo.mem.InMemoryPasteRepo
 import pl.crejk.tempbin.util.SecurityUtil
 import java.util.concurrent.TimeUnit
 
+@ObsoleteCoroutinesApi
 class PasteServiceTest : BehaviorSpec({
-    val service = PasteService(InMemoryPasteRepo())
+    Given("a service") {
+        val service = PasteService(InMemoryPasteRepo())
 
-    val createdPaste = service.createPaste(PasteDTO("test"))
-    val paste = service.getPaste(createdPaste.id).get(3, TimeUnit.SECONDS)!!
+        When("created a paste") {
+            val createdPaste = service.createPaste(PasteDTO("test"))
 
-    paste.id shouldBe createdPaste.id
+            Then("paste should be in repo") {
+                val paste = service.getPaste(createdPaste.id)
+
+                paste?.id shouldBe createdPaste.id
+            }
+
+            Then("paste should be removed from repo") {
+                service.removePaste(createdPaste.id)
+
+                service.getPaste(createdPaste.id) shouldBe null
+            }
+        }
+    }
 })
