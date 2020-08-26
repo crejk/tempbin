10f1b785004a52e63585b1689ca6eaec0d1d1a7b
crejk <crejk@ctemplar.com>
2020-08-26 14:41:49 +0200

    tidy up

--- src/main/kotlin/pl/crejk/tempbin/paste/PasteService.kt
+++ src/main/kotlin/pl/crejk/tempbin/paste/PasteService.kt
@@ -1,11 +1,11 @@
 package pl.crejk.tempbin.paste
 
+import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
 import kotlinx.coroutines.*
 import pl.crejk.tempbin.fp.*
 import pl.crejk.tempbin.paste.repo.PasteRepo
 import pl.crejk.tempbin.util.SecurityUtil
-import pl.crejk.tempbin.util.SuspendingCache
 import java.time.LocalDateTime
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -15,16 +15,11 @@ class PasteService(
     private val repo: PasteRepo
 ) {
 
-    private val compute = newFixedThreadPoolContext(4, "service-thread-pool")
+    private val executor = newFixedThreadPoolContext(4, "service-thread-pool")
 
-    private val cache = SuspendingCache<PasteId, Paste>(
-        Caffeine.newBuilder()
-            .executor(this.compute.executor)
+    private val cache = Caffeine.newBuilder()
             .expireAfterAccess(5, TimeUnit.MINUTES)
-            .buildAsync<PasteId, Paste>()
-    ) {
-        this.repo.findPaste(it)
-    }
+            .build<PasteId, Paste>()
 
     suspend fun createPaste(newPaste: PasteDTO): PasteResult {
         val pasteId = UUID.randomUUID()
@@ -43,38 +38,42 @@ class PasteService(
             newPaste.deleteAfterReading
         )
 
-        return withContext(this.compute) {
-            if (repo.savePaste(paste)) {
-                cache.put(paste.id, paste)
-                PasteResult(paste.id.toString(), password)
-            } else {
-                PasteResult.EMPTY
-            }
+        return if (withContext(executor) { repo.savePaste(paste) }) {
+            cache.put(paste.id, paste)
+
+            PasteResult(paste.id.toString(), password)
+        } else {
+            PasteResult.EMPTY
         }
     }
 
+
     suspend fun getPaste(id: PasteId): Either<PasteError, Paste> =
-        this.cache.get(id)
+        this.cache.getOrMap(id, { this.repo.findPaste(id) })
             .either(PasteError.NOT_FOUND)
             .filterOrElse({ !it.isExpired() }, { PasteError.EXPIRED })
 
     suspend fun getPasteContent(id: PasteId, password: String): Either<PasteError, String> =
         this.getPaste(id)
-            .flatMap { this.decrypt(password, it.salt, it.content)
-            .either(PasteError.WRONG_PASSWORD) }
+            .flatMap {
+                this.decrypt(password, it.salt, it.content)
+                    .either(PasteError.WRONG_PASSWORD)
+            }
 
     private fun decrypt(password: String, salt: String, encryptedContent: EncryptedContent) = Try {
         SecurityUtil.prepareTextEncryptor(password, salt).decrypt(encryptedContent.value)
     }
 
-    suspend fun removePaste(id: PasteId) = coroutineScope {
-        async(compute) {
+    suspend fun removePaste(id: PasteId) =
+        withContext(this.executor) {
             repo.removePaste(id)
-        }.invokeOnCompletion {
+        }.also {
             cache.invalidate(id)
         }
-    }
 }
+
+private inline fun <K : Any, V> Cache<K, V>.getOrMap(key: K, f: (K) -> V?) =
+    this.getIfPresent(key) ?: f(key)?.also { this.put(key, it) }
 
 data class PasteResult(
     val id: String,
