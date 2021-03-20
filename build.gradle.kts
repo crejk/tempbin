327ca87b597523d05e6a7f689e4bcfa883c8219c
crejk <crejk@ctemplar.com>
2021-03-20 15:59:54 +0100

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,14 +20,12 @@ repositories {
     jcenter()
 }
 
-val ktorVersion = "1.5.1"
-val kotestVersion = "4.2.5"
+val ktorVersion = "1.5.2"
+val kotestVersion = "4.4.3"
 val vavrVersion = "0.10.2"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.springframework.security:spring-security-crypto:5.3.4.RELEASE")
-    implementation("com.github.ben-manes.caffeine:caffeine:2.8.5")
 
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
@@ -38,11 +36,13 @@ dependencies {
     implementation("io.vavr:vavr-kotlin:$vavrVersion")
     implementation("io.vavr:vavr-jackson:$vavrVersion")
 
-    implementation("org.hashids:hashids:1.0.3")
+    implementation("org.springframework.security:spring-security-crypto:5.3.4.RELEASE")
+    implementation("com.github.ben-manes.caffeine:caffeine:2.8.5")
     implementation("commons-codec:commons-codec:1.14")
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-ktor:$kotestVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
 }
 
